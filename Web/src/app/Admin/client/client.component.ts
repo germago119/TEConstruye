6b87763e67158f7665738434982a97808e3d5ae9
Roger Valderrama <roger.andres18@gmail.com>
2019-11-02 00:19:18 -0600

    Formly alfa

--- Web/src/app/Admin/client/client.component.ts
+++ Web/src/app/Admin/client/client.component.ts
@@ -14,40 +14,77 @@ export class ClientComponent {
 
   fields: FormlyFieldConfig[] = [
     {
-      className: '',
       key: 'nombre',
       type: 'input',
       templateOptions: {
-        placeholder: 'nombre del cliente',
+        placeholder: 'John',
+        label: 'Nombre: ',
+        description: 'Nombre debe ser texto',
+        required: true,
         addonLeft: {
           class: 'icon ion-ios-contact bg-transparent border-primary',
+          size: '22'
+        }
+      }
+    },
+    {
+      key: 'apellido1',
+      type: 'input',
+      templateOptions: {
+        placeholder: 'Doe',
+        label: 'Primer Apellido: ',
+        description: 'Apellido debe ser texto',
+        required: true,
+        addonLeft: {
+          class: 'icon ion-ios-bowtie bg-transparent border-primary',
           size: '16'
-        },
-        label: 'Nombre: '
+        }
+      }
+    },
+    {
+      key: 'apellido2',
+      type: 'input',
+      templateOptions: {
+        placeholder: '---',
+        label: 'Segundo Apellido: ',
+        description: 'Apellido debe ser texto',
+        required: true,
+        addonLeft: {
+          class: 'icon ion-ios-color-filter bg-transparent border-primary',
+          size: '16'
+        }
       }
     },
     {
       key: 'cedula',
       type: 'input',
       templateOptions: {
+        type: 'number',
         placeholder: '',
+        label: 'Cedula',
+        description: 'Cedula debe tener 9 digitos',
+        required: true,
+        min: 100000000,
+        max: 999999999,
         addonLeft: {
-          class: 'icon ion-md-heart'
-        },
-
-        label: 'One add-on on both side (left: icon, right: text)'
+          class: 'syss icon ion-ios-finger-print bg-transparent border-primary'
+        }
       }
     },
     {
-      key: 'right',
+      key: 'telefono',
       type: 'input',
       templateOptions: {
-        placeholder: "Nice, isn't it??",
-
-        addonRight: {
-          class: 'icon ion-md-heart'
-        },
-        label: 'One add-on on the right (icon)'
+        type: 'number',
+        placeholder: '',
+        label: 'Telefono',
+        description: 'Telefono debe tener 8 digitos',
+        required: true,
+        min: 10000000,
+        max: 99999999,
+        addonLeft: {
+          class: 'icon ion-ios-call bg-transparent border-primary syss'
+        }
       }
     }
   ];
