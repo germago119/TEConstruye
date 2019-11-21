4643e229f45d0eb07db8c937b242966021abde15
gebv0896 <gebv0896@gmail.com>
2019-11-21 10:20:51 -0600

    Archivo de TEConstruye_FUNCTIONS completo

--- SQL/TEConstruye_FUNCTIONS.sql
+++ SQL/TEConstruye_FUNCTIONS.sql
@@ -1,3 +1,19 @@
+CREATE OR REPLACE FUNCTION GenerarPresupuesto()
+RETURNS TABLE(Nombre_Proyecto VARCHAR,
+			 Nombre_Etapa VARCHAR,
+			 presupuesto INT)
+AS $$
+BEGIN
+	RETURN QUERY
+	SELECT PR.Nombre AS Nombre_Proyecto,E.Nombre AS Nombre_Etapa,E.presupuesto
+	FROM PROYECTO AS PR LEFT JOIN OBRA AS O ON PR.Nombre=O.Nombre_Proyecto
+	LEFT JOIN ETAPA_OBRA AS EO ON EO.Nombre_Obra=O.Nombre 
+	LEFT JOIN ETAPA AS E ON E.Codigo=EO.Codigo_Etapa 
+	WHERE EO.Nombre_Obra IS NOT NULL;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
 CREATE OR REPLACE FUNCTION GenerarPlanilla(OUT Nombre_Empleado VARCHAR,
 			 OUT Apellido_Empleado VARCHAR,
 			 OUT Nombre_Proyecto VARCHAR,
